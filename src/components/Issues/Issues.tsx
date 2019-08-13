26bdd8a91231944eed6b4aab468467597422df36
Jesslyn Landgren <jlandgren@insiten.com>
2019-08-12 22:55:53 -0400

    adding upvote button, refactoring layout

--- src/components/Issues/Issues.tsx
+++ src/components/Issues/Issues.tsx
@@ -1,4 +1,4 @@
-import { Button, Layout, Menu, Modal, PageHeader, Tag } from "antd";
+import { Button, Icon, Layout, Menu, Modal, Typography } from "antd";
 import * as _ from "lodash";
 import React from "react";
 import styled from "styled-components";
@@ -11,21 +11,53 @@ export interface IIssue {
 }
 
 const { Content, Sider } = Layout;
+const { Title } = Typography;
+
+const StyledIssuesContainer = styled.div`
+	background-color: #fff;
+`;
 
 const StyledContent = styled(Content)`
 	background-color: #fff;
 `;
 
-const StyledIssueListTitle = styled.div`
+const StyledIssueToolbar = styled.div`
+	display: flex;
+	justify-content: space-between;
 	padding: .75em;
-	font-size: 1.1em;
+`;
+
+const StyledIssueListTitle = styled.div`
+	font-size: 1.5em;
 	font-weight: bold;
 `;
 
-const StyledIssueToolbar = styled.div`
+const StyledIssueHeader = styled.div`
 	display: flex;
-	justify-content: flex-end;
-	padding: .75em;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1.5em;
+`;
+
+const StyledIssueActions = styled.div`
+	display: flex;
+	align-items: center;
+
+	> * {
+		margin-left: .5em;
+	}
+`;
+
+const StyledLikedIcon = styled(Icon)`
+`;
+
+const StyledUpvotes = styled.div`
+	margin-right: .5em;
+	font-size: 1.25em;
+`;
+
+const StyledUpvoteIcon = styled.span`
+	margin-right: .2em;
 `;
 
 export default class Issues extends React.Component<any, any> {
@@ -55,73 +87,89 @@ export default class Issues extends React.Component<any, any> {
 	public render() {
 		return (
 			<Layout>
-				<Sider style={{ backgroundColor: "#fff" }}>
-					<StyledIssueListTitle className="listTitle">
-						Oh git! I accidentally...
-					</StyledIssueListTitle>
-					<Menu
-						mode="inline"
-						onSelect={this.selectIssue}
-					>
-						{this.state.issues.map((issue: IIssue) => {
-							return (
-								<Menu.Item key={issue.id}>
-									<span>
-										{issue.title}
-									</span>
-								</Menu.Item>
-							);
-						})}
-					</Menu>
-				</Sider>
-				<StyledContent>
+				<StyledIssuesContainer>
 					<StyledIssueToolbar>
+						<StyledIssueListTitle className="listTitle">
+							Oh git! I accidentally...
+						</StyledIssueListTitle>
 						<Button
 							type="primary"
-							ghost={true}
 							onClick={() => this.toggleCreateModal(true)}
 						>
-								Add New
+							Add New
 						</Button>
 					</StyledIssueToolbar>
-						{this.state.selectedIssue &&
-							<PageHeader
-								title={this.state.selectedIssue.title}
-								subTitle={this.state.selectedIssue.title}
-								tags={<Tag color="red">Warning</Tag>}
-								extra={[
-									<Button
-										key="2"
-										type="danger"
-										ghost={true}
-										icon="delete"
-										onClick={() => this.deleteIssue(this.state.selectedIssue.id)}
-									/>,
-									<Button
-										key="1"
-										type="primary"
-										icon="edit"
-									/>,
-								]}
-							/>
-						}
-					<Modal
-						visible={this.state.isCreatingIssue}
-						title="Create New Issue"
-						onCancel={() => this.toggleCreateModal(false)}
-						onOk={()  => this.createIssue({title: "Test Issue Yay", votes: 0})}
-						okText="Save"
-					>
-						test text
-					</Modal>
-				</StyledContent>
+					<Layout>
+						<Sider>
+							<Menu
+								mode="inline"
+								onSelect={this.selectIssue}
+							>
+								{this.state.issues.map((issue: IIssue) => {
+									return (
+										<Menu.Item key={issue.id}>
+											<span>
+												{issue.title}
+											</span>
+										</Menu.Item>
+									);
+								})}
+							</Menu>
+						</Sider>
+						<StyledContent>
+							{this.state.selectedIssue &&
+								<div>
+									<StyledIssueHeader>
+										<div>
+											<Title level={4}>...{this.state.selectedIssue.title}</Title>
+										</div>
+										<StyledIssueActions>
+											<StyledUpvotes>
+												<StyledUpvoteIcon>
+													{(true) ?
+														<StyledLikedIcon type="heart" theme="filled" />
+													:
+														<Icon type="heart" theme="outlined" />
+													}
+												</StyledUpvoteIcon>
+												<span>{this.state.selectedIssue.votes}</span>
+											</StyledUpvotes>
+											<Button
+												type="danger"
+												ghost={true}
+												icon="delete"
+												onClick={() => this.deleteIssue(this.state.selectedIssue.id)}
+											/>
+											<Button
+												type="primary"
+												ghost={true}
+												icon="edit"
+												onClick={() => this.editIssue(this.state.selectedIssue.id)}
+											/>
+										</StyledIssueActions>
+									</StyledIssueHeader>
+								</div>
+							}
+							<Modal
+								visible={this.state.isCreatingIssue}
+								title="Create New Issue"
+								onCancel={() => this.toggleCreateModal(false)}
+								onOk={()  => this.createIssue({title: "Test Issue Yay", votes: 0})}
+								okText="Save"
+							>
+								test text
+							</Modal>
+						</StyledContent>
+					</Layout>
+				
+				</StyledIssuesContainer>
 			</Layout>
+			
 
 		);
 	}
 
 	public selectIssue = (menuSelection: any) => {
-		console.log("selecting issue", menuSelection);
 		const issue = _.find(this.state.issues, {id: menuSelection.key});
 		this.setState({selectedIssue: issue});
 	}
@@ -139,5 +187,9 @@ export default class Issues extends React.Component<any, any> {
 	public deleteIssue = (id: string) => {
 		const issueRef = firebase.database().ref(`/issues/${id}`);
 		issueRef.remove();
+	}
+
+	public editIssue = (id: string) => {
+		console.log("do nothing");
 	}
 }
